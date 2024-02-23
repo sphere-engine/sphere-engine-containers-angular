c250600ae959cdb7338a028a07556b2f51608395
olewna <dajcie.suba2002@gmail.com>
2024-02-23 13:38:56 +0100

    aside html

--- workspace_integration/src/app/app.module.ts
+++ workspace_integration/src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { WorkspaceComponent } from './components/workspace/workspace.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AsideComponent, WorkspaceComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
